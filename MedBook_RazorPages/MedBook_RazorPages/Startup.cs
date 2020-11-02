c99f7bc80ccc2158878744d8699bd54ff2e39dae
aioanedragos <aioane.costin@yahoo.com>
2020-11-02 13:48:46 +0200

    db ramane null

--- MedBook_RazorPages/MedBook_RazorPages/Startup.cs
+++ MedBook_RazorPages/MedBook_RazorPages/Startup.cs
@@ -1,29 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MedBook_RazorPages.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace MedBook_RazorPages
 {
-    public class Startup
+    public class Startup        
     {
+
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+      
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            /*MvcOptions ceva = new MvcOptions();
+            ceva.EnableEndpointRouting = false;*/
+
+            var connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<DatabaseContext>(opstions => opstions.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +58,8 @@ namespace MedBook_RazorPages
             {
                 endpoints.MapRazorPages();
             });
+
+            app.UseMvc();
         }
     }
 }
