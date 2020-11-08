f8e90e51aa76db0ad47d510b0b38713ce6308f47
iSizar <irinel.manolache96@gmail.com>
2020-11-08 20:25:11 +0200

    Updated DB Script Generator

--- DataBase Scripts/DataBaseCreator.sql
+++ DataBase Scripts/DataBaseCreator.sql
@@ -11,36 +11,40 @@ Password varchar(200),
 UserType int
 );
 
+create table Location
+(
+id int primary key identity(1, 1),
+City varchar(30),
+StreenName varchar(30),
+StreenNr varchar(5),
+);
+
+
 create table MedicalService
 (
 id int primary key identity(1, 1),
 UserId int,
+LocationId int,
 Name varchar(50),
 Description varchar(50),
+TargetBodySystem varchar(50),
 DayStartTime time, 
 DayEndTime time,
 AppDefaultMinutes time,
 
-FOREIGN KEY (UserId) REFERENCES Users(id)
-);
-
-create table Location
-(
-id int primary key identity(1, 1),
-MedicalServiceId int,
-City varchar(30),
-StreenName varchar(30),
-StreenNr varchar(5),
-FOREIGN KEY (MedicalServiceId) REFERENCES MedicalService(id)
+FOREIGN KEY (UserId) REFERENCES Users(id),
+FOREIGN KEY (LocationId) REFERENCES Location(id)
 );
 
 create table Review
 (
 id int primary key identity(1, 1),
+UserId int,
 MedicalServiceId int,
 Comment varchar(50),
 Rating int
 
+FOREIGN KEY (userId) REFERENCES Users(id),
 FOREIGN KEY (MedicalServiceId) REFERENCES MedicalService(id)
 );
 
