87160527c54b053981c3eca385955e0bfd1c3caa
danibranch <danybrench33@gmail.com>
2020-11-16 23:11:59 +0200

    added some razor pages

---
+++ MedBook_RazorPages/MedBook_RazorPages/Pages/NewMedicalService.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MedBook_RazorPages.Models;
+
+namespace MedBook_RazorPages.Pages
+{
+    public class NewMedicalServiceModel : PageModel
+    {
+        private readonly DatabaseContext _context;
+
+        public NewMedicalServiceModel(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public MedicalService MedicalService { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.MedicalService.Add(MedicalService);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
